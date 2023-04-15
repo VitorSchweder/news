873b96a0f3d2f0f810972df247b777c766c424f5
Vitor Schweder <vitor.scw@gmail.com>
2023-04-15 18:30:24 -0300

    fix: database config

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL', 'postgres://ujjxvxnsaadhhl:4111b66fbd5ea19c21464a895bcd42dffd359d000b9a265ab10605ed25097b83@ec2-34-197-91-131.compute-1.amazonaws.com:5432/d5sjkg2vq6ar3o'),
-            'host' => env('DB_HOST', 'ec2-34-197-91-131.compute-1.amazonaws.com'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'd5sjkg2vq6ar3o'),
-            'username' => env('DB_USERNAME', 'ujjxvxnsaadhhl'),
-            'password' => env('DB_PASSWORD', '4111b66fbd5ea19c21464a895bcd42dffd359d000b9a265ab10605ed25097b83'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
